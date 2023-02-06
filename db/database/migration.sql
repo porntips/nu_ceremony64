6bc13a1d533bbe706204cfe31e117f367e3d1620
porntips <81725697+porntips@users.noreply.github.com>
2023-02-06 15:23:56 +0700

    commit

--- db/database/migration.sql
+++ db/database/migration.sql
@@ -13,20 +13,3 @@ CREATE TABLE ceremonyDB(
     ceremonyprefix varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2,
     ceremony boolean
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-CREATE TABLE `diarys`
-(
-    id   bigint auto_increment,
-    image varchar(255) NOT NULL,
-    note varchar(255) NOT NULL,
-    diary_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-);
-CREATE TABLE `usernames`
-(
-    username varchar(255) NOT NULL,
-    identifier varchar(255) NOT NULL,
-    firstname varchar(255) NOT NULL,
-    lastname varchar(255) NOT NULL
-);
